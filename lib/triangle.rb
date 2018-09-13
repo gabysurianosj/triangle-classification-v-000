1533b5488e215e1a42b0739f3938fc83f777b507
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-13 08:14:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,5 +2,19 @@ class Triangle
 
   def initialize(side_1, side_2, side_3)
   end 
+  
+  def kind 
+    if valid? 
+      if @triangle_sides.uniq.length == 1 
+        return :equilateral 
+      elsif @triangle_sides.uniq.length == 2 
+        return :isosceles 
+      else 
+        return :scalene 
+      end 
+    else 
+      raise TriangleError 
+    end 
+  end 
 
 end
